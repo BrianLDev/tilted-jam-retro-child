dc8093db8b3c524fd364f9d1461d00a30c04fd1b
BMarcelus <bmdizon@uci.edu>
2020-08-15 16:03:55 -0700

    player prefab

--- Assets/JumpState.cs
+++ Assets/JumpState.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetBoolOnEnter : StateMachineBehaviour
+public class JumpState : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+      PlayerMovement.instance.DoJump();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
