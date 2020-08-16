bf0b1038489e2d50fe7c7938c3d98f8f86155699
BMarcelus <bmdizon@uci.edu>
2020-08-15 21:27:19 -0700

    star piece

---
+++ Assets/Bob.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob : MonoBehaviour
+{
+    public float frq;
+    public float dist;
+    private Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      pos.y = Mathf.Cos(Time.time*frq)*dist;
+        transform.localPosition = pos;
+    }
+}
